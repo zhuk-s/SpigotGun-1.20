207a180478317794cd3c5dd48a65e84b8b9f2a6d
zhuk_s <dimonsavran@gmail.com>
2024-08-02 08:58:42 +0200

    0.1

--- src/main/java/me/zhuk/spigotGuns/ArmsHandler.java
+++ src/main/java/me/zhuk/spigotGuns/ArmsHandler.java
@@ -1,2 +1,21 @@
-package me.zhuk.spigotGuns;public class ArmsHandler {
+package me.zhuk.spigotGuns;
+
+import org.bukkit.NamespacedKey;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
+
+import javax.naming.Name;
+import java.util.logging.Handler;
+
+public class ArmsHandler {
+    public static void Handler(Gun gun, Player player) {
+        ItemStack gunItem = new ItemStack(gun.getGunMaterial());
+
+        ItemMeta meta = gunItem.getItemMeta();
+
+        meta.getPersistentDataContainer().set(NamespacedKey.fromString("gun_name"), PersistentDataType.STRING, gun.name());
+    }
 }
--- src/main/java/me/zhuk/spigotGuns/Gun.java
+++ src/main/java/me/zhuk/spigotGuns/Gun.java
@@ -1,4 +1,54 @@
 package me.zhuk.spigotGuns;
 
+import org.bukkit.Material;
+
 public enum Gun {
+
+    AKM(10, 30, "AKM", Material.CYAN_DYE),
+    GLOCK(5, 8, "Glock", Material.INK_SAC);
+
+
+    private double damage;
+    private int bulletsNum;
+    private String name;
+    private Material gunMaterial;
+
+    Gun (double damage, int bulletsNum, String name, Material gunMaterial) {
+        this.damage = damage;
+        this.bulletsNum = bulletsNum;
+        this.name = name;
+        this.gunMaterial = gunMaterial;
+    }
+
+    public double getDamage() {
+        return damage;
+    }
+
+    public void setDamage(double damage) {
+        this.damage = damage;
+    }
+
+    public int getBulletsNum() {
+        return bulletsNum;
+    }
+
+    public void setBulletsNum(int bulletsNum) {
+        this.bulletsNum = bulletsNum;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Material getGunMaterial() {
+        return gunMaterial;
+    }
+
+    public void setGunMaterial(Material gunMaterial) {
+        this.gunMaterial = gunMaterial;
+    }
 }
